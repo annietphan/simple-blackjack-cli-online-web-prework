5aeb2ed3117e39f7502b0749988b4d097025f212
Annie Phan <anniephan.atp@gmail.com>
2019-02-28 21:21:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,5 +57,7 @@ def runner
   # code runner here
   welcome
   card_total = initial_round
-  hit?(card_total)
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
 end
